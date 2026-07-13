231887bae822282ca4a2f1ef39aef065a8b0ae51
fravila08 <fr4v1l4@gmail.com>
2026-07-13 14:12:27 -0700

    assert for typing with tests

--- test_main.py
+++ test_main.py
@@ -5,7 +5,14 @@ def test_pytest_connection():
     assert True
 
 def test_calculator_add():
-    calculator = Calculator()
-    assert calculator.add(1,3) == 4
-    assert calculator.add(5,3) == 8
-    assert calculator.add(11,14) == 25
+    try:
+        calculator = Calculator()
+        assert calculator.add(1,3) == 4
+        assert calculator.add(5,3) == 8
+        assert calculator.add(11,14) == 25
+        calculator.add('11', '12')
+    except Exception as e:
+        assert e.args[0] == "Both arguments must be an integer"
+    
+
+
