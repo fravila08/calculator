22b241e0a0af5d0049daa32f35a8aeb2a6051de2
fravila08 <fr4v1l4@gmail.com>
2026-07-13 14:30:51 -0700

    multiply feature comp

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 class Calculator:
 
     def add(self, arg_1:int, arg_2:int)->int:
-        if not isinstance(arg_1, int) or not isinstance(arg_2, int):
-            raise ValueError("Both arguments must be an integer")
+        self.check_type(arg_1, arg_2)
         return arg_1 + arg_2
     
     def multiply(self, arg_1:int, arg_2:int)-> int:
+        self.check_type(arg_1, arg_2)
+        return arg_1 * arg_2
+    
+    @classmethod
+    def check_type(cls, arg_1, arg_2) -> bool | ValueError:
         if not isinstance(arg_1, int) or not isinstance(arg_2, int):
             raise ValueError("Both arguments must be an integer")
-        return arg_1 * arg_2
+        return True
