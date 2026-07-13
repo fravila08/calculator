7e194e68c03f3bcc64e6198a6d1451f1d4158910
fravila08 <fr4v1l4@gmail.com>
2026-07-13 13:44:03 -0700

    create test for adding method

--- test_main.py
+++ test_main.py
@@ -1,4 +1,11 @@
 import pytest
+from main import Calculator
 
 def test_pytest_connection():
     assert True
+
+def test_calculator_add():
+    calculator = Calculator()
+    assert calculator.add(1,3) == 4
+    assert calculator.add(5,3) == 8
+    assert calculator.add(11,14) == 25
